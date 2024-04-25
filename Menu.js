38f7e29758f6ddaabc0cde2beb637aac48edc09e
ohhitherefriendo <163308972+ohhitherefriendo@users.noreply.github.com>
2024-04-25 05:00:23 -0500

    Add files via upload

---
+++ Menu.js
@@ -0,0 +1,226 @@
+const Menu = [
+    {
+        name: 'Lucky Bento',
+        uid: 'luckybento',
+        price: 500,
+        display: true,
+        items: 'Omurice, Onigiri, Meowcaroons, UwU-Pop',
+        itemType: 'combo'
+    },
+    {
+        name: '9 Lives',
+        uid: '9lives',
+        price: 300,
+        display: true,
+        image: 'images/items/9LivesCombo.png',
+        items: 'UwU Bowl, Dumplings, Flan, Strawberry Milk',
+        itemType: 'combo'
+    },
+    {
+        name: 'Cat Burglar',
+        uid: 'catburglar',
+        price: 300,
+        display: true,
+        image: 'images/items/CatBurglarCombo.png',
+        items: 'Chicken Cat-Su, Miso Soup, Taiyaki, Boba Tea',
+        itemType: 'combo'
+    },
+    {
+        name: 'Pawcakes Meal',
+        uid: 'pawcakesmeal',
+        price: 150,
+        display: true,
+        image: 'images/items/pawcakesmeal.png',
+        items: 'Pawcakes, Sakura Tea, Cupcat',
+        itemType: 'combo'
+    },
+    {
+        name: 'Kitty Meal',
+        uid: 'kittymeal',
+        price: 75,
+        display: true,
+        image: 'images/items/kittymeal.png',
+        items: 'Cupcat, Boba Tea',
+        itemType: 'combo'
+    },
+    {
+        name: 'Pawliday Meal',
+        uid: 'pawlidaymeal',
+        price: 500,
+        display: true,
+        image: 'images/items/promo/pawlidaymeal.png',
+        items: 'Holiday Pawffles, Meowced Pie, Santas Cookies, Hot Choco-Cat',
+        itemType: 'combo'
+    },
+    {
+        name: 'Omurice',
+        uid: 'omurice',
+        price: 300,
+        display: true,
+        image: 'images/items/omurice.png',
+        items: '',
+        itemType: 'main'
+    },
+    {
+        name: 'UwU Bowl',
+        uid: 'uwubowl',
+        price: 200,
+        display: true,
+        image: 'images/items/uwubowl.png',
+        items: '',
+        itemType: 'main'
+    },
+    {
+        name: 'Chicken Cat-Su',
+        uid: 'chickencatsu',
+        price: 200,
+        display: true,
+        image: 'images/items/chickencatsu.png',
+        items: '',
+        itemType: 'main'
+    },
+    {
+        name: 'Pawcakes',
+        uid: 'pawcakes',
+        price: 100,
+        display: true,
+        image: 'images/items/pawcake.png',
+        items: '',
+        itemType: 'main'
+    },
+    {
+        name: 'Onigiri',
+        uid: 'onigiri',
+        price: 100,
+        display: true,
+        image: 'images/items/onigiri.png',
+        items: '',
+        itemType: 'side'
+    },
+    {
+        name: 'Dumplings',
+        uid: 'dumplings',
+        price: 50,
+        display: true,
+        image: 'images/items/dumplings.png',
+        items: '',
+        itemType: 'side'
+    },
+    {
+        name: 'Miso Soup',
+        uid: 'misosoup',
+        price: 50,
+        display: true,
+        image: 'images/items/misosoup.png',
+        items: '',
+        itemType: 'side'
+    },
+    {
+        name: 'Meow-caroons',
+        uid: 'mewocaroons',
+        price: 100,
+        display: true,
+        image: 'images/items/meowcaroons.png',
+        items: '',
+        itemType: 'dessert'
+    },
+    {
+        name: 'Flan',
+        uid: 'flan',
+        price: 50,
+        display: true,
+        image: 'images/items/flan.png',
+        items: '',
+        itemType: 'dessert'
+    },
+    {
+        name: 'Taiyaki',
+        uid: 'taiyaki',
+        price: 50,
+        display: true,
+        image: 'images/items/taiyaki.png',
+        items: '',
+        itemType: 'dessert'
+    },
+    {
+        name: 'Cupcat',
+        uid: 'cupcat',
+        price: 50,
+        display: true,
+        image: 'images/items/cupcat.png',
+        items: '',
+        itemType: 'dessert'
+    },
+    {
+        name: 'UwU-Pop',
+        uid: 'uwupop',
+        price: 100,
+        display: true,
+        image: 'images/items/uwupop.png',
+        items: '',
+        itemType: 'drink'
+    },
+    {
+        name: 'Strawberry Milk',
+        uid: 'strawberrymilk',
+        price: 50,
+        display: true,
+        image: 'images/items/strawberrymilk.png',
+        items: '',
+        itemType: 'drink'
+    },
+    {
+        name: 'Boba Tea',
+        uid: 'bobatea',
+        price: 50,
+        display: true,
+        image: 'images/items/bobatea.png',
+        items: '',
+        itemType: 'drink'
+    },
+    {
+        name: 'Sakura Tea',
+        uid: 'sakuratea',
+        price: 50,
+        display: true,
+        image: 'images/items/sakura.png',
+        items: '',
+        itemType: 'drink'
+    },
+    {
+        name: '5x Pawcakes',
+        uid: '5pawcakes',
+        price: 500,
+        display: true,
+        image: 'images/items/pawcake.png',
+        items: '',
+        itemType: 'business Deal'
+    },
+    {
+        name: '10x Pawcakes',
+        uid: '10pawcakes',
+        price: 1000,
+        display: true,
+        image: 'images/items/pawcake.png',
+        items: '',
+        itemType: 'business Deal'
+    },
+    {
+        name: '20x Pawcakes',
+        uid: '20pawcakes',
+        price: 2000,
+        display: true,
+        image: 'images/items/pawcake.png',
+        items: '',
+        itemType: 'business Deal'
+    },
+    {
+        name: '50x Pawcakes',
+        uid: '50pawcakes',
+        price: 5000,
+        display: true,
+        image: 'images/items/pawcake.png',
+        items: '',
+        itemType: 'business Deal'
+    }
+];
---
+++ script.js
@@ -0,0 +1,368 @@
+const Menu = [
+    {
+        name: 'Crescent Combo',
+        uid: 'crescentmoon',
+        price: 30,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1225242592925585498/image.png?ex=6629a596&is=66173096&hm=6b815f0420fa35b62be8388856c937dc04ba9dd12eb37ecfa2235644a8cac523&',
+        items: '1 Drink/1 Treat',
+        itemType: 'combo'
+    },
+    {
+        name: 'Full Moon Combo',
+        uid: 'FullMoon',
+        price: 60,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1225242592925585498/image.png?ex=6629a596&is=66173096&hm=6b815f0420fa35b62be8388856c937dc04ba9dd12eb37ecfa2235644a8cac523&',
+        items: '2 Drinks/2 Treats',
+        itemType: 'combo'
+    },
+    {
+        name: 'Old Moon Combo',
+        uid: 'OldMoon',
+        price: 90,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1225242592925585498/image.png?ex=6629a596&is=66173096&hm=6b815f0420fa35b62be8388856c937dc04ba9dd12eb37ecfa2235644a8cac523&',
+        items: '4 Drinks/4 Treats',
+        itemType: 'combo'
+    },    
+    {
+        name: 'Lunar Eclipse',
+        uid: 'Eclipse',
+        price: 110,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1225242592925585498/image.png?ex=6629a596&is=66173096&hm=6b815f0420fa35b62be8388856c937dc04ba9dd12eb37ecfa2235644a8cac523&',
+        items: '1 of Everything',
+        itemType: 'combo'
+    },
+    {
+        name: 'Sausage Roll',
+        uid: 'Roll',
+        price: 20,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1207564227745808454/IMG_0945.png?ex=6629eed3&is=661779d3&hm=b7da3d95016052a6e64d0f2d0b7a37132a319522ac88ab70bf9c2f7038cdece3&',
+        items: '',
+        itemType: 'Savory'
+    },
+    {
+        name: 'Ham & Cheese Croissant',
+        uid: 'Ham&Cheese',
+        price: 20,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1207564229163618354/IMG_0943.png?ex=6629eed3&is=661779d3&hm=4bdc96dce815871eda82da86f4151d9777eba66aa7154041a49867020cb20acd&',
+        items: '',
+        itemType: 'Savory'
+    },
+    {
+        name: 'BEC Croissant',
+        uid: 'BEC',
+        price: 20,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1207564229163618354/IMG_0943.png?ex=6629eed3&is=661779d3&hm=4bdc96dce815871eda82da86f4151d9777eba66aa7154041a49867020cb20acd&',
+        items: '',
+        itemType: 'Savory'
+    },
+    {
+    name: 'Moonlit Smoothie Bowl',
+        uid: 'Moonlit',
+        price: 20,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1225244727486578768/1227228147385106432/IMG_1079.png?ex=6627a447&is=66152f47&hm=57ba76e6f9758493ee35a7dc32b8bd6b6b9f731363d6b4f1a0a579a4974332fa&',
+        items: '',
+        itemType: 'Sweet'
+    },
+    {
+        name: 'Twin Lunar Cakepops',
+        uid: 'TwinLunar',
+        price: 20,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1216909492495585343/IMG_1044.png?ex=66270448&is=66148f48&hm=8e3f2f088ba646940d68e69eb874cd1658560dc5380ab12a78b8f55af885d51e&',
+        items: '',
+        itemType: 'Sweet'
+    },
+    {
+        name: 'Cheesecake Brownie',
+        uid: 'Brownie',
+        price: 20,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1219857745046409357/IMG_1068.png?ex=6628838e&is=66160e8e&hm=e0754803a67e07886d0eaa167893d9e9b66a8529b9c455897c7398751ab615dd&',
+        items: '',
+        itemType: 'Sweet'
+    },
+    {
+        name: 'Purrfect Cinnamon Roll',
+        uid: 'Cinnamon',
+        price: 20,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1211598325569290270/IMG_0978.png?ex=662626df&is=6613b1df&hm=2f2648de4dd065ce77f63a5dcdbe891b5f4d710654b53559f6031023e7198525&',
+        items: '',
+        itemType: 'Sweet'
+    },    
+    {
+        name: 'Premonitions',
+        uid: 'premo',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1227897153796702228/Premonitions.png?ex=662a1356&is=66179e56&hm=940dd8a182f745ee683eb17bbf2783cefacc2d3f239e484c217fde4e723838d8&',
+        items: '',
+        itemType: 'Iced Coffee'
+    },
+    {
+        name: 'Nitro Bitches Brew',
+        uid: 'NBB',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1222159758967050260/IMG_1070.png?ex=6627a8f9&is=661533f9&hm=c14d334c1ddcdc258bc475ee4806793bb5a0670ad5b1c94bbeb234ded84a3777&',
+        items: '',
+        itemType: 'Iced Coffee'
+    },
+    {
+        name: 'Shadow Work',
+        uid: 'Shadow',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1225244727486578768/1227229739954077709/IMG_1080.png?ex=6627a5c2&is=661530c2&hm=8a699f94794481a729029ad18b070141f2b3559cd1587e0d709947eb9c04f0ac&',
+        items: '',
+        itemType: 'Iced Coffee'
+    },
+    {
+        name: 'Bitches Brew',
+        uid: 'Bitches',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1206625740221841478/1213653502405115955/brew.png?ex=66246667&is=6611f167&hm=967de1a47192d5b73ea53beb970938d5bd336ede79d6a091e13b15e44d4d7afb&',
+        items: '',
+        itemType: 'Hot Coffee'
+    },
+    {
+        name: 'Vanta Black',
+        uid: 'Vanta',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1206625740221841478/1213653583405391872/Vanta_Black.png?ex=6624667a&is=6611f17a&hm=3d64694e6bf1a3755f0f8bf8b963b5bd8b17213353f145a0555c1e93ec5ed051&',
+        items: '',
+        itemType: 'Hot Coffee'
+    },
+    {
+        name: 'Blood Moon Blend',
+        uid: 'BloodMoon',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1206625740221841478/1213653501448691782/blood_moon_blend.png?ex=66246667&is=6611f167&hm=fea0429f0f5047418ac1086463c43a60a6395edd445a8394ec392790a712fd2d&',
+        items: '',
+        itemType: 'Hot Coffee'
+    },
+    {
+        name: 'Aurora Lights',
+        uid: 'Aurora',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1206625740221841478/1213653500580335679/Aurora_Lights.png?ex=66246667&is=6611f167&hm=db1776f29f1145ec73b798e349eacefb067ddcd07196f18f0db2091531609205&',
+        items: '',
+        itemType: 'Hot Coffee'
+    },
+    {
+        name: 'The Floyd',
+        uid: 'Floyd',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1206625740221841478/1213653581862014976/The_Floyd.png?ex=6624667a&is=6611f17a&hm=a57bb2ecacf268e1ed3fcb38a7d005703af0fa8fcdcc412e3c5591cbe8b882d1&',
+        items: '',
+        itemType: 'Hot Coffee'
+    },
+    {
+        name: 'Midnight Dreams',
+        uid: 'Dreams',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1206625740221841478/1213655077613735986/Midnight_Dreams.png?ex=662467df&is=6611f2df&hm=6c8d1ec1456d8a22c6c994d60dc1f4e4531c453ff8f2026eb56f64af5f8fdb4a&',
+        items: '',
+        itemType: 'Tea'
+    },
+    {
+        name: 'Spellbound',
+        uid: 'Spell',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/1206625740221841478/1213653499863375912/SpellBound.png?ex=66246666&is=6611f166&hm=c2fccb3dbcb2a9c520436ff39d48c6f872d4aa4e4dc206b208416079a7c3507a&',
+        items: '',
+        itemType: 'Tea'
+    },
+    {
+        name: 'Incantation',
+        uid: 'Incan',
+        price: 15,
+        display: true,
+        image: 'https://cdn.discordapp.com/attachments/990535131049635840/1215206451816046643/IMG_1030.png?ex=6620d233&is=660e5d33&hm=27645c68d8e7d5693a25fc7543cd270fca609f9959f224073fd267ae5da00893&',
+        items: '',
+        itemType: 'Tea'
+    },
+];
+
+function loadItems() {
+    let itemList = document.getElementById('itemsContainer');
+    let lastItemType = "";
+    Menu.forEach(menuItem => {
+        if (menuItem.display == true) {
+            if (menuItem.itemType  !== lastItemType) {
+                itemList.innerHTML += `<div class="col-md-12 col-sm-12 col-12">${capitalizeFirstLetter(menuItem.itemType)}s</div>`;
+                lastItemType = menuItem.itemType;
+            }
+
+            itemList.innerHTML += 
+            `<div class="col-md-3 col-sm-3 col-3 text-center">
+                <figure class="card card-product" onclick="addItem('${menuItem.uid}','${menuItem.name}',${menuItem.price},'${menuItem.image}','${menuItem.items}')">
+                    <div class="img-wrap">
+                        <img draggable="false" src=${menuItem.image}>
+                    </div>
+                    <figcaption class="info-wrap">
+                        <span class="title h6">${menuItem.name}</span>
+                        <div class="action-wrap">
+                            <span class="btn btn-primary disabled btn-sm"> <i
+                                    class="fa fa-cart-plus"></i>$${menuItem.price} </span>
+                        </div> <!-- action-wrap -->
+                    </figcaption>
+                </figure> <!-- card // -->
+            </div> <!-- col // -->`
+        }
+    });
+}
+
+function addItem(uid, name, price, image, items) {
+    var receiptItem = 'receipt-item-' + uid;
+    var priceItem = 'price-item-' + uid;
+    var quantityItem = 'quantity-item-' + uid;
+    if (document.getElementById(receiptItem)) {
+        let orderPriceString = document.getElementById(priceItem).innerText.replace(/\D/g, '');
+        let orderQuantityString = document.getElementById(quantityItem).innerText.replace(/\D/g, '');
+        let orderPrice = Number(orderPriceString);
+        let orderQuantity = Number(orderQuantityString);
+        orderPrice += 1 * price
+        orderQuantity += 1
+        document.getElementById(priceItem).innerText = '$' + orderPrice
+        document.getElementById(quantityItem).innerText = orderQuantity
+        totalPrice();
+    }
+    else {
+        let receiptList = document.getElementById('tbody');
+        let title = `<h6 class="title text-truncate menu-item">${name}</h6>`;
+        if (items !== '') {
+            title += `<h8 class="title text-truncate staff-help"><i>${items}</i></h8>`;
+        }
+        receiptList.innerHTML += 
+        `<tr class="receipt-item" id="receipt-item-${uid}">
+            <td>
+                <figure class="media">
+                    <div class="img-wrap"><img draggable="false" src="${image}"
+                        class="img-thumbnail img-xs"></div>
+                    <figcaption class="media-body">
+                        ${title}
+                    </figcaption>
+                </figure>
+            </td>
+            <td class="text-center">
+                <div class="m-btn-group m-btn-group--pill btn-group mr-2" role="group"
+                    aria-label="...">
+                    <button type="button" class="m-btn btn btn-default" onclick="removeItem('${uid}',${price},1)"><i
+                            class="fa fa-minus"></i></button>
+                    <button type="button" class="m-btn btn btn-default quantity" id="${quantityItem}" disabled>1</button>
+                    <button type="button" class="m-btn btn btn-default" onclick="addItem('${uid}','${name}',${price},'${image}')"><i
+                            class="fa fa-plus"></i></button>
+                </div>
+            </td>
+            <td>
+                <div class="price-wrap">
+                    <var class="price" id="${priceItem}">$${price}</var>
+                </div>
+            </td>
+            <td class="text-right">
+                <span class="btn btn-outline-danger" onclick="deleteItem('${uid}')"> <i class="fa fa-trash"></i></span>
+            </td>
+        </tr>`
+        totalPrice();
+    }
+}
+
+function removeItem(uid, price, quantity) {
+    if (document.getElementById('receipt-item-' + uid)) {
+        let orderPriceString = document.getElementById('price-item-' + uid).innerText.replace(/\D/g, '');
+        let orderQuantityString = document.getElementById('quantity-item-' + uid).innerText.replace(/\D/g, '');
+        let orderPrice = Number(orderPriceString);
+        let orderQuantity = Number(orderQuantityString);
+        if (orderQuantity > 1) {
+            orderPrice -= price
+            orderQuantity -= quantity
+            document.getElementById('price-item-' + uid).innerText = '$' + orderPrice
+            document.getElementById('quantity-item-' + uid).innerText = orderQuantity
+            totalPrice();
+        }
+    }  
+}
+function deleteItem(item) {
+    document.getElementById('receipt-item-' + item).remove();
+    totalPrice();
+}
+
+function resetTill(item) {
+    document.getElementById('tbody').innerHTML = '';
+    totalPrice();
+}
+
+function applyDiscount(discount) {
+    let discountPercent = (discount / 100);
+    let total = document.getElementById('totalCost').innerText.replace(/\D/g, '');
+    let discountedTotal = Math.ceil(total - (total * discountPercent));
+    document.getElementById('totalCost').innerText = '$' + discountedTotal;
+}
+
+function totalPrice() {
+    var elements = document.getElementsByClassName('price');
+    if (elements.length == 0) {
+        document.getElementById('totalCost').innerText = '$' + 0
+        generateComment();
+    }
+    else {
+        prices = [];
+        for (var i = 0; i < elements.length; ++i) {
+            var priceElement = elements[i].innerHTML.replace(/\D/g, '');
+            var price = Number(priceElement)
+            prices.push(price);
+            let total = prices.reduce(function (acc, val) { return acc + val; }, 0)
+            document.getElementById('totalCost').innerText = '$' + total
+            generateComment();
+        }
+    }
+}
+
+function generateComment() {
+    document.getElementById('orderComment').innerText = "";
+    document.getElementById('hiddenComment').value = "";
+    var itemComments = []
+    var itemQuantity = []
+    var quantity = document.getElementsByClassName("quantity");
+    for (var a = 0; a < quantity.length; a++) {
+        itemQuantity.push(quantity[a].innerText);
+    }
+    var receiptItems = document.getElementsByClassName("menu-item");
+    for (var i = 0; i < receiptItems.length; i++) {
+        itemComments.push(quantity[i].innerText + "x " + receiptItems[i].innerText);
+    }
+    document.getElementById('orderComment').innerHTML = `<button class="btn btn-primary btn-lg" id='copyToClipBoard' data-clipboard-target="#hiddenComment">
+        <span class="h5">Copy Order Comment</span>
+        </button> <br> 
+        <div style="color:white;">`+ 
+        itemComments.join(' ') + 
+        "</div>";
+    document.getElementById('hiddenComment').value = itemComments.join(' ');
+    let clipboard = new ClipboardJS('#copyToClipBoard');
+    clipboard.on('success', function(e) {
+        e.clearSelection();
+});
+
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+loadItems();
